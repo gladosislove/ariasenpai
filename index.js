71f92a35ebb2381ff9e4af4ddbc3bbe3b784b467
gladosislove <33029439+gladosislove@users.noreply.github.com>
2019-03-14 21:40:00 +0000

    Update index.js

--- index.js
+++ index.js
@@ -26,10 +26,9 @@ bot.on('ready', () => {
 bot.on('message', async message => {
     if (triggerWords.some(word => message.content.includes(word))) {
 
-        let memberChannel = message.channel;
         let userMessage = message.content()
 
-        bot.channels.get('554302850574974996').send(`${message.author.tag} in ${memberChannel}: ${userMessage}`);
+        bot.channels.get('554302850574974996').send(`${message.author.tag}: ${userMessage}`);
     }
 });
 
