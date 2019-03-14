72045cd71812db53a90e93ed1f310b6f1cb2f30f
gladosislove <33029439+gladosislove@users.noreply.github.com>
2019-03-14 21:32:54 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const PREFIX = '-'
 
+const triggerWords = ['kill myself',
+    'death',
+    'alive',
+    'live',
+    'want to die',
+    'knife',
+    'hurt myself',
+    'suicide',
+    'kill me',
+    'noose',
+    'hang',
+    'slit',
+    'blood',
+    'dying'
+];
+
+bot.on('ready', () => {
+    bot.user.setGame('your heart <3')
+    console.log('Online.')
+});
+
+bot.on('message', async message => {
+    if (triggerWords.some(word => message.content.includes(word))) {
+
+        let memberChannel = message.memberChannel.get()
+        let userMessage = message.content()
+
+        bot.channels.get('554302850574974996').send(`${message.author.tag} in ${memberChannel}: ${userMessage}`);
+    }
+});
+
+bot.login(process.env.BOT_TOKEN);
